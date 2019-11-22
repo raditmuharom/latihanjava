0761ddadf1cbb315a93203026209add224c69c26
raditmuharom <radit1muharom@gmail.com>
2019-11-22 15:44:35 +0700

    latihanjava

---
+++ 1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Radit Muharom
+ * NIM   : A2.1900138
+ * Kelas : TI-1C
+ * 
+ * @author raditmuharom
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : Radit Muharom");
+        System.out.println("ALAMAT\t\t         : Baginda ");
+        System.out.println("TEMPAT LAHIR\t\t : Bogor ");
+        System.out.println("TANGGAL LAHIR\t\t : 26 Maret 2001");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Olahraga");
+        System.out.println("MAKES\t\t: Mie lemonilo");
+        System.out.println("MIKES\t\t: Fanta");
+    }
+    
+}
---
+++ 15.Logika/src/l15/logika/L15Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l15.logika;
+
+/**
+ ** NAMA  : Radit Muharom
+ * NIM   : A2.1900138
+ * Kelas : TI-1C
+  */
+public class L15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = true;
+       System.out.println("a && b = " + (a && b));
+       System.out.println("a || b = " + (a || b));
+       System.out.println("a !a = " +  !a);
+       System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
